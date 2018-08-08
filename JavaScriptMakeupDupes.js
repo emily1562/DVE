7040be38c629ca929087fbdc59546aeac987d417
AyeeitsVivi <41652964+AyeeitsVivi@users.noreply.github.com>
2018-08-08 16:22:26 -0700

    Dupes

--- JavaScriptMakeupDupes.js
+++ JavaScriptMakeupDupes.js
@@ -12,7 +12,7 @@ function FunctionFace() {
   if (document.getElementById("FaceText").innerHTML != ""){
   document.getElementById("FaceText").innerHTML = "";
   }
-  else{document.getElementById("FaceText").innerHTML = `<p><h3>contour</h3></p> NYX highlight and contour HD palette $25, compared to Kat Von D contour pallete $46. <p><h3>foundation</h3></p> Foundation stick Revol photoready Instafix foundation stick $11, compared to Bobby Browns 46$ <br> Maybelline Fit Me foundation $7.99, compared to Mac foundation $20 <p><h3> concealer </h3></p> Revlon Makeup Revolution concealer 7$, compared to Tarte Shape Tate concealer $27 <p><h3>Powder</p></h3> Maybelline Master fiz setting powder 7.99$,compared to Makeup Forever HD microfinishing powder $23  <p><h3> Setting Spray </h3></p> Loreal Paria infallible setting spray $14,compared to Urban Decay all nighter makeup setting spray $30`
+  else{document.getElementById("FaceText").innerHTML = `<p><h3>contour</h3></p> <img src="DUPES_NYXcontourPalette_V.jpg"> NYX highlight and contour HD palette $25, compared to Kat Von D contour pallete $46. <p><h3>foundation</h3></p> <img src="FoundationStick.jpg"> Revlon photoready Instafix foundation stick $11, compared to Bobby Browns 46$ <br> <img src="MaybellineFitMefoundation.jpg"> Maybelline Fit Me foundation $7.99, compared to Mac foundation $20 <p><h3> concealer </h3></p> <img src="RevlonMakeupRevolutionConcealer.jpg"> Revlon Makeup Revolution concealer 7$, compared to Tarte Shape Tate concealer $27 <p><h3>Other</p></h3> <img src="MaybellineSettingPowder.png"> Maybelline Master fiz setting powder 7.99$,compared to Makeup Forever HD microfinishing powder $23 <br> <img src="LorealPariaSettingSpray.jpg"> Loreal Paria infallible setting spray $14,compared to Urban Decay all nighter makeup setting spray $30`
     ;}
 }
   //cheek
@@ -21,7 +21,7 @@ function FunctionFace() {
     document.getElementById("CheeksText").innerHTML = "";
     }
     else{document.getElementById("CheeksText").innerHTML =
-    `<p><h3>highlighter</h3><p> Colour pop Pearlized highlighter $8 compared to Becca Shimmering Skin Perfector pressed highlighter $38​<br>Elf Cosmetic baked shade Enchanted $3, compared to Becca's Champagne pop $38​ <br> NYX Born to Glow liquid Illuminator $8,compared to Baccas shimmering skin perfector liquid highlighter $41​<br>Wet n wild Megaglow $5 compared to Beccas Opal highlighting powder $38​
+    `<p><h3>highlighter</h3><p> Colour pop Pearlized highlighter $8 compared to Becca Shimmering Skin Perfector pressed highlighter $38​<br>Elf Cosmetic baked shade Enchanted $3, compared to Becca's Champagne pop $38​ <br> NYX Born to Glow liquid Illuminator $8,compared to Baccas shimmering skin perfector liquid highlighter $41​
     <p><h3>bronzer</h3>​</p>Romel natural bronzer $4 compared to Benefit cosmetics hoola bronzer $29<br>NYX Professional makeup bronzer $7 compared to Benefit Cosmetics Hoola Bronzer $29​
     <p><h3>Blush​</p></h3> Milani Baked Blush $7 compared to Nars Orgasm $29​<br>Maybeline Fit Me blush medium pink $5 compared to Mac Dainty $25​<br> Elf Studio blush in Berry Merry $3 compared to Urban Decays afterglow Rapture $26​`
   ;}
@@ -32,7 +32,7 @@ function FunctionLips() {
   document.getElementById("LipText").innerHTML = "";
   }
   else{document.getElementById("LipText").innerHTML =
-  `<p><h3>Matte​ Lipcream</p></h3>Colour pop matte lip cream $6 compared to Kiley Jenners 30$ Lipkit ​<br> Maybelline Fuschsia Ectasy $8,compared to Too Faced its Happening $21<br> Colorpop mars $6, compared to Too Faced its Happening $21 ​​<br>NYX Tres Mechas $6, compared to koko k $30 <br> NYX monte Carlo compared to Kat Von D outlaw​ <br> Maybelline gone greige $7.49 compared to Huda flirt $20​
+  `<p><h3>Matte​ Lipcream</p></h3>Colour pop matte lip cream $6 compared to Kiley Jenners 30$ Lipkit ​<br> Maybelline Fuschsia Ectasy $8,compared to Too Faced its Happening $21<br> Colorpop mars $6, compared to Too Faced its Happening $21 ​​<br>NYX Tres Mechas $6, compared to koko k $30 <br> NYX monte Carlo compared to Kat Von D outlaw​
  <p><h3>lip liner​</p></h3> Rimmel London 1000 kisses lip liner $3 compared to Smash Box always sharp lip liner $20 ​
  <p><h3>Lip gloss​</p></h3>Colourpop Bad Habit $7,compared to Dose of colors berry me $18​<br>Colour pop dopey $7, compared to Stila patina $22
  ​<p><h3>Lipstick</p></h3>Elf Natural Nymph $3 compared to Mac Creme D'nude lipstick $19​<br>Wet n wild Fergie $6 compared to Urban decay Venom $18​<br> Wet n Wild Sugar Plum $4 compared to Mac Rebel $19​<br>​Revlon Plumalicious $5,compared to Mac Amorous $19​<br>Elf Classy Lipstick $3, NYX Sky Pink lipstick $4, compared to Mac angel $19​
