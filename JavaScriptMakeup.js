01b46251dfeee35f81e29e7e6cf7b41164f6b577
AyeeitsVivi <41652964+AyeeitsVivi@users.noreply.github.com>
2018-08-08 14:31:52 -0700

    javaMakeup

---
+++ JavaScriptMakeup.js
@@ -0,0 +1,43 @@
+//test
+
+function FunctionTest() {
+  //if (document.getElementById("TestText").innerHTML != ""){
+//  document.getElementById("TestText").innerHTML = "";
+  //}
+//  else{
+
+  document.getElementById("TestText").innerHTML = "test";
+}
+
+//Face
+function FunctionFace() {
+  if (document.getElementById("FaceText").innerHTML != ""){
+  document.getElementById("FaceText").innerHTML = "";
+  }
+  else{document.getElementById("FaceText").innerHTML ="facee info"
+  ;}
+}
+  //cheek
+  function FunctionCheeks() {
+    if (document.getElementById("CheeksText").innerHTML != ""){
+    document.getElementById("CheeksText").innerHTML = "";
+    }
+    else{document.getElementById("CheeksText").innerHTML ="put in info cheeks"
+    ;}
+}
+  //lips
+  function FunctionLips() {
+    if (document.getElementById("LipText").innerHTML != ""){
+    document.getElementById("LipText").innerHTML = "";
+    }
+    else{document.getElementById("LipText").innerHTML ="put in info lips"
+    ;}
+}
+  //eyes
+  function FunctionEyes() {
+    if (document.getElementById("EyesText").innerHTML != ""){
+    document.getElementById("EyesText").innerHTML = "";
+    }
+    else{document.getElementById("EyesText").innerHTML ="put in infor eyes"
+    ;}
+}
