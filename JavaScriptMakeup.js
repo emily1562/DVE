8320e4b9c06c38d0334b25b72d09c80ab2d351df
AyeeitsVivi <Vierylis@outlook.com>
2018-08-09 08:41:49 -0700

    makeup

--- JavaScriptMakeup.js
+++ JavaScriptMakeup.js
@@ -13,7 +13,24 @@ function FunctionFace() {
   if (document.getElementById("FaceText").innerHTML != ""){
   document.getElementById("FaceText").innerHTML = "";
   }
-  else{document.getElementById("FaceText").innerHTML ="facee info"
+  else{document.getElementById("FaceText").innerHTML = `<p><h3>Primer</h3>​</p>
+  Benefit Cosmetics, POREfessional Face Primer Price 49$
+  <br> Find it <a href="/phttps://www.sephora.com/product/the-porefessional-face-primer-P264900?skuId=1665074&icid2=products%20grid:p264900:product">Sephora</a>.
+​<br><br> Too Faced, Hangover Replenishing Face PrimerPrice 34$
+<br>​Find it in <a href="https://www.sephora.com/product/hangover-replenishing-face-primer-P387589?icid2=products%20grid:p387589:product">Sephora</a>.
+
+<p><h3>Foundation</h3>​<a>
+<br>​Fenty Beauty, Pro Filt’r Soft Matte Longwear Foundation Price 34$<br>
+<a href="https://www.sephora.com/product/pro-filtr-soft-matte-longwear-foundation-P87985432?skuId=1925304&icid2=products%20grid:p87985432:product">Sephora</a>.
+<br><br> Too Faced, Born This Way Foundation Price 39$ <br>
+<a href="https://www.sephora.com/product/born-this-way-P397517?skuId=1711910&icid2=products%20grid:p397517:product">Sephora</a>.
+
+<p><h3>Concealer</h3>​<a>
+Nars, Radiant Creamy Concealer Price 30$
+<br>​<a href="https://www.sephora.com/product/radiant-creamy-concealer-P377873?icid2=products%20grid:p377873:product">Sephora</a>.
+
+
+`
   ;}
 }
   //cheek
