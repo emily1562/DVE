23eb734a999adcc4edaa382ed1a91b11b0a1e9f4
dellinag <dellinag@yahoo.com>
2018-08-09 15:47:53 -0700

    formatting

--- JavaScriptMakeupDupes.js
+++ JavaScriptMakeupDupes.js
@@ -1,18 +1,49 @@
 //MakeupDupes
+
  //test
- function FunctionTest() {
-   if (document.getElementById("TestText").innerHTML != ""){
-   document.getElementById("TestText").innerHTML = "";
-   }
-   else{document.getElementById("TestText").innerHTML = "test";
-   }
-}
+//  function FunctionTest() {
+//    if (document.getElementById("TestText").innerHTML != ""){
+//    document.getElementById("TestText").innerHTML = "";
+//    }
+//    else{document.getElementById("TestText").innerHTML = "test";
+//    }
+// }
+
   //Face
 function FunctionFace() {
   if (document.getElementById("FaceText").innerHTML != ""){
   document.getElementById("FaceText").innerHTML = "";
   }
-  else{document.getElementById("FaceText").innerHTML = `<p><h3>contour</h3></p> <img src="DUPES_NYXcontourPalette_V.jpg"> NYX highlight and contour HD palette $25, compared to Kat Von D contour pallete $46. <p><h3>foundation</h3></p> <img src="FoundationStick.jpg"> Revlon photoready Instafix foundation stick $11, compared to Bobby Browns 46$ <br> <img src="MaybellineFitMefoundation.jpg"> Maybelline Fit Me foundation $7.99, compared to Mac foundation $20 <p><h3> concealer </h3></p> <img src="RevlonMakeupRevolutionConcealer.jpg"> Revlon Makeup Revolution concealer 7$, compared to Tarte Shape Tate concealer $27 <p><h3>Other</p></h3> <img src="MaybellineSettingPowder.png"> Maybelline Master fiz setting powder 7.99$,compared to Makeup Forever HD microfinishing powder $23 <br> <img src="LorealPariaSettingSpray.jpg"> Loreal Paria infallible setting spray $14,compared to Urban Decay all nighter makeup setting spray $30 `
+  else{
+    document.getElementById("FaceText").innerHTML =
+    `<p><h3>contour</h3></p><div class= "polaroid"> <img src="DUPES_NYXcontourPalette_V.jpg" alt= "NYX Cream Contour Palette" style="width:100%">
+    <div class="container">
+    <p> NYX highlight and contour HD palette <br> Price: $25, compared to Kat Von D contour pallete $46.</p> </div> </div>
+
+
+    '<p><h3>foundation</h3></p> <div class= "polaroid"> <img src="FoundationStick.jpg" alt= "Revlon Photoready Instafix foundation" style="width:100%">
+    <div class="container">
+    <p>Revlon Photoready Instafix foundation stick <br> Price: $11, compared to Bobby Browns $46. </p> </div> </div>
+
+    <div class="polaroid"> <img src="MaybellineFitMefoundation.jpg" alt= "Maybelline Fit Me Foundation" style="width:100%">
+    <div class="container">
+    <p> Maybelline Fit Me foundation <br> Price: $7.99, compared to Mac foundation $20 </p> </div> </div>
+
+    '<p><h3> concealer </h3></p> <div class= "polaroid"> <img src="RevlonMakeupRevolutionConcealer.jpg" alt= "Revlon Concealer" style="width:100">
+    <div class="container">
+     <p> Revlon Makeup Revolution Concealer <br> Price: $7, compared to Tarte Shape Tate concealer $27.</p> </div> </div>
+
+     '<p><h3>Other</p></h3> <div class="polaroid"> <img src="MaybellineSettingPowder.png" alt= "Maybelline Setting Powder" style= "width:100">
+     <div class="container">
+      <p> Maybelline Master fiz setting powder <br> Price: $7.99,compared to Makeup Forever HD microfinishing powder $23.</p> </div> </div>
+
+       <br> <img src="LorealPariaSettingSpray.jpg"> Loreal Paria infallible setting spray $14,compared to Urban Decay all nighter makeup setting spray $30`
+  //   ```<p><h3>contour</h3></p>
+  // <div class= "polaroid"> <img src="DUPES_NYXcontourPalette_V.jpg"> alt= "NYX Cream Contour Palette" style="width:100%">
+  //   <div class="container">
+  //   </div>
+  // </div>
+  //   NYX highlight and contour HD palette $25, compared to Kat Von D contour pallete $46. <p><h3>foundation</h3></p> <img src="FoundationStick.jpg"> Revlon photoready Instafix foundation stick $11, compared to Bobby Browns 46$ <br> <img src="MaybellineFitMefoundation.jpg"> Maybelline Fit Me foundation $7.99, compared to Mac foundation $20 <p><h3> concealer </h3></p> <img src="RevlonMakeupRevolutionConcealer.jpg"> Revlon Makeup Revolution concealer 7$, compared to Tarte Shape Tate concealer $27 <p><h3>Other</p></h3> <img src="MaybellineSettingPowder.png"> Maybelline Master fiz setting powder 7.99$,compared to Makeup Forever HD microfinishing powder $23 <br> <img src="LorealPariaSettingSpray.jpg"> Loreal Paria infallible setting spray $14,compared to Urban Decay all nighter makeup setting spray $30```
     ;}
 }
   //cheek
@@ -21,7 +52,7 @@ function FunctionFace() {
     document.getElementById("CheeksText").innerHTML = "";
     }
     else{document.getElementById("CheeksText").innerHTML =
-    `<p><h3>highlighter</h3><p> <img src="ColorPopHighlight.png"> pop Pearlized-Scrumptious highlighter $8,compared to Becca Shimmering Skin Perfector pressed highlighter $38​<br> <img src="ElfShadeEnchanted .png"> Elf Cosmetic baked shade Enchanted $3, compared to Becca's Champagne pop $38​<br>  <img src="NYXliquidIlluminator.jpg"> NYX Born to Glow liquid Illuminator $8,compared to Baccas shimmering skin perfector liquid highlighter $41​
+    `<p><h3>highlighter</h3><p> <img src="ColorPopHighlight.png"> pop Pearlized-Scrumptious highlighter $8,compared to Becca Shimmering Skin Perfector pressed highlighter $38​<br> <img src="ElfShadeEnchanted .png"> Elf Cosmetic baked shade Enchanted $3, compared to Beccas Champagne pop $38​<br>  <img src="NYXliquidIlluminator.jpg"> NYX Born to Glow liquid Illuminator $8,compared to Baccas shimmering skin perfector liquid highlighter $41​
     <p><h3>bronzer</h3>​</p> <img src="RomelBronzer.jpg">Romel natural bronzer $4 compared to Benefit cosmetics hoola bronzer $29<br> <img src="NYXBronzer.jpg"> NYX Professional makeup bronzer $7 compared to Benefit Cosmetics Hoola Bronzer $29​
     <p><h3>Blush​</p></h3> <img src="MilaniBlush.jpg"> Milani Baked Blush $7 compared to Nars Orgasm $29​<br>  <img src="MaybelineBlushPink.jpg"> Maybeline Fit Me blush medium pink $5 compared to Mac Dainty $25​<br>  <img src="ElfBlushBM.jpg"> Elf Studio blush in Berry Merry $3 compared to Urban Decays afterglow Rapture $26​`
   ;}
@@ -34,7 +65,7 @@ function FunctionLips() {
   else{document.getElementById("LipText").innerHTML =
   `<p><h3>Matte​ Lipcream</p></h3> <img src="MaybellineEctasy.jpg"> Maybelline Fuschsia Ectasy $8,compared to Too Faced its Happening $21<br> <img src="NYXTresMechas.jpg"> NYX Tres Mechas $6, compared to koko k $30 <br> <img src="NYXMonteCarlo.jpg">NYX monte Carlo compared to Kat Von D outlaw​
  <p><h3>Lip gloss​</p></h3> <img src="ColourpopBadHabit.jpg"> Colourpop Bad Habit $7,compared to Dose of colors berry me $18​<br> <img src="ColourDopey.jpg"> Colour pop dopey $7, compared to Stila patina $22
- ​<p><h3>Lipstick</p></h3> <img src="Dnude.jpg"> Elf Natural Nymph $3 compared to Mac Creme D'nude lipstick $19 ​<br> <img src="SugarPlum.jpg"> Wet n Wild Sugar Plum $4 compared to Mac Rebel $19​<br> <img src="RevlonPlumalicious.jpg"> ​Revlon Plumalicious $5,compared to Mac Amorous $19​<br> <img src="ElfClassy.jpg"> Elf Classy Lipstick $3 <br> <img src="SkyPink.jpg"> NYX Sky Pink lipstick $4, compared to Mac angel $19​
+ ​<p><h3>Lipstick</p></h3> <img src="Dnude.jpg"> Elf Natural Nymph $3 compared to Mac Creme Dnude lipstick $19 ​<br> <img src="SugarPlum.jpg"> Wet n Wild Sugar Plum $4 compared to Mac Rebel $19​<br> <img src="RevlonPlumalicious.jpg"> ​Revlon Plumalicious $5,compared to Mac Amorous $19​<br> <img src="ElfClassy.jpg"> Elf Classy Lipstick $3 <br> <img src="SkyPink.jpg"> NYX Sky Pink lipstick $4, compared to Mac angel $19​
  <p><h3>metallic​</h3></p>  <img src="ColourpopLightsOut.jpg"> Colour pop Lights Out $6, compared to Anastasias Beverly Hills Gilded $16​<br> <img src="ChromaticAddict.jpg"> Milani Chromatic Addict $9,Compared to Kiley Jenners King K $18​<br>  <img src="MilaniMaterialistic.jpg"> Milani Materialistic $9,compared to Kiley Jenners Reign $18​<br>
  <p><h3>lip crayons</h3></p>  <img src="SoniaKashuk.jpg"> Sonia Kashuk velvet lip crayons $7.99, compared to Nars Velvet matte lip pencil $26​​<br><img src="NYXEnamored.jpg"> NYX Enamored $6.50, compared to Gerard Underworld $20​`;}
 }
