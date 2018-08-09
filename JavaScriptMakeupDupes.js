ddb329737e00b5e77d1a9b9f14fbc8acba7218d6
AyeeitsVivi <Vierylis@outlook.com>
2018-08-09 03:52:07 -0700

    dupess Done

--- JavaScriptMakeupDupes.js
+++ JavaScriptMakeupDupes.js
@@ -21,9 +21,9 @@ function FunctionFace() {
     document.getElementById("CheeksText").innerHTML = "";
     }
     else{document.getElementById("CheeksText").innerHTML =
-    `<p><h3>highlighter</h3><p> <img src="ColorPopHighlight.png"> Colour pop Pearlized-Scrumptious highlighter $8,compared to Becca Shimmering Skin Perfector pressed highlighter $38​<br> !!!!"" <img src="ColorPopHighlight.png"> Elf Cosmetic baked shade Enchanted $3, compared to Becca's Champagne pop $38​ <br>  <img src="ColorPopHighlight.png"> !!!! NYX Born to Glow liquid Illuminator $8,compared to Baccas shimmering skin perfector liquid highlighter $41​
-    <p><h3>bronzer</h3>​</p>  <img src="ColorPopHighlight.png"> !!! Romel natural bronzer $4 compared to Benefit cosmetics hoola bronzer $29<br> <img src="ColorPopHighlight.png"> !!!! NYX Professional makeup bronzer $7 compared to Benefit Cosmetics Hoola Bronzer $29​
-    <p><h3>Blush​</p></h3> <img src="ColorPopHighlight.png"> !!!! Milani Baked Blush $7 compared to Nars Orgasm $29​<br>  <img src="ColorPopHighlight.png"> !!!! Maybeline Fit Me blush medium pink $5 compared to Mac Dainty $25​<br>  <img src="ColorPopHighlight.png"> !!! Elf Studio blush in Berry Merry $3 compared to Urban Decays afterglow Rapture $26​`
+    `<p><h3>highlighter</h3><p> <img src="ColorPopHighlight.png"> Colour pop Pearlized-Scrumptious highlighter $8,compared to Becca Shimmering Skin Perfector pressed highlighter $38​<br> <img src="ElfShadeEnchanted .png"> Elf Cosmetic baked shade Enchanted $3, compared to Becca's Champagne pop $38​ <br>  <img src="NYXliquidIlluminator.jpg"> NYX Born to Glow liquid Illuminator $8,compared to Baccas shimmering skin perfector liquid highlighter $41​
+    <p><h3>bronzer</h3>​</p> <img src="RomelBronzer.jpg">Romel natural bronzer $4 compared to Benefit cosmetics hoola bronzer $29<br> <img src="NYXBronzer.jpg"> NYX Professional makeup bronzer $7 compared to Benefit Cosmetics Hoola Bronzer $29​
+    <p><h3>Blush​</p></h3> <img src="MilaniBlush.jpg"> Milani Baked Blush $7 compared to Nars Orgasm $29​<br>  <img src="MaybelineBlushPink.jpg"> Maybeline Fit Me blush medium pink $5 compared to Mac Dainty $25​<br>  <img src="ElfBlushBM.jpg"> Elf Studio blush in Berry Merry $3 compared to Urban Decays afterglow Rapture $26​`
   ;}
 }
   //lips
@@ -32,12 +32,11 @@ function FunctionLips() {
   document.getElementById("LipText").innerHTML = "";
   }
   else{document.getElementById("LipText").innerHTML =
-  `<p><h3>Matte​ Lipcream</p></h3> <img src="ColorPopHighlight.png"> !!!! Colour pop matte lip cream $6 compared to Kiley Jenners 30$ Lipkit ​<br>  <img src="ColorPopHighlight.png"> !!!! Maybelline Fuschsia Ectasy $8,compared to Too Faced its Happening $21<br> <img src="ColorPopHighlight.png"> !!!! Colorpop mars $6, compared to Too Faced its Happening $21 ​​<br>  <img src="ColorPopHighlight.png"> !!!! NYX Tres Mechas $6, compared to koko k $30 <br> <img src="ColorPopHighlight.png"> !!!! NYX monte Carlo compared to Kat Von D outlaw​
- <p><h3>lip liner​</p></h3>  <img src="ColorPopHighlight.png"> !!!! Rimmel London 1000 kisses lip liner $3 compared to Smash Box always sharp lip liner $20 ​
- <p><h3>Lip gloss​</p></h3> <img src="ColorPopHighlight.png"> !!!! Colourpop Bad Habit $7,compared to Dose of colors berry me $18​<br> <img src="ColorPopHighlight.png"> !!!! Colour pop dopey $7, compared to Stila patina $22
- ​<p><h3>Lipstick</p></h3> <img src="ColorPopHighlight.png"> !!!! Elf Natural Nymph $3 compared to Mac Creme D'nude lipstick $19​<br>  <img src="ColorPopHighlight.png"> !!!! Wet n wild Fergie $6 compared to Urban decay Venom $18​<br>  <img src="ColorPopHighlight.png"> !!!! Wet n Wild Sugar Plum $4 compared to Mac Rebel $19​<br>  <img src="ColorPopHighlight.png"> !!!! ​Revlon Plumalicious $5,compared to Mac Amorous $19​<br>Elf Classy Lipstick $3, NYX Sky Pink lipstick $4, compared to Mac angel $19​
- <p><h3>metallic​</h3></p>  <img src="ColorPopHighlight.png"> !!!! Colourpop Lights out $6, compared to Anastasias Beverly Hills Gilded $16​<br> <img src="ColorPopHighlight.png"> !!!! Milani Chromatic Addict $9,Compared to Kiley Jenners King K $18​<br>  <img src="ColorPopHighlight.png"> !!!! Milani Materialistic $9,compared to Kiley Jenners Reign $18​<br> <img src="ColorPopHighlight.png"> !!!! NYX Enamored $6.50, compared to Gerard Underworld $20​
- <p><h3>lip crayons</h3></p>  <img src="ColorPopHighlight.png"> !!!! Sonia Kashuk velvet lip crayons $7.99, compared to Nars Velvet matte lip pencil $26​`;}
+  `<p><h3>Matte​ Lipcream</p></h3> <img src="MaybellineEctasy.jpg"> Maybelline Fuschsia Ectasy $8,compared to Too Faced its Happening $21<br> <img src="NYXTresMechas.jpg"> NYX Tres Mechas $6, compared to koko k $30 <br> <img src="NYXMonteCarlo.jpg">NYX monte Carlo compared to Kat Von D outlaw​
+ <p><h3>Lip gloss​</p></h3> <img src="ColourpopBadHabit.jpg"> Colourpop Bad Habit $7,compared to Dose of colors berry me $18​<br> <img src="ColourDopey.jpg"> Colour pop dopey $7, compared to Stila patina $22
+ ​<p><h3>Lipstick</p></h3> <img src="Dnude.jpg"> Elf Natural Nymph $3 compared to Mac Creme D'nude lipstick $19 ​<br> <img src="SugarPlum.jpg"> Wet n Wild Sugar Plum $4 compared to Mac Rebel $19​<br> <img src="RevlonPlumalicious.jpg"> ​Revlon Plumalicious $5,compared to Mac Amorous $19​<br> <img src="ElfClassy.jpg"> Elf Classy Lipstick $3 <br> <img src="SkyPink.jpg"> NYX Sky Pink lipstick $4, compared to Mac angel $19​
+ <p><h3>metallic​</h3></p>  <img src="ColourpopLightsOut.jpg"> Colour pop Lights Out $6, compared to Anastasias Beverly Hills Gilded $16​<br> <img src="ChromaticAddict.jpg"> Milani Chromatic Addict $9,Compared to Kiley Jenners King K $18​<br>  <img src="MilaniMaterialistic.jpg"> Milani Materialistic $9,compared to Kiley Jenners Reign $18​<br>
+ <p><h3>lip crayons</h3></p>  <img src="SoniaKashuk.jpg"> Sonia Kashuk velvet lip crayons $7.99, compared to Nars Velvet matte lip pencil $26​​<br><img src="NYXEnamored.jpg"> NYX Enamored $6.50, compared to Gerard Underworld $20​`;}
 }
 
   //eyes
@@ -46,8 +45,8 @@ function FunctionEyes() {
   document.getElementById("EyesText").innerHTML = "";
   }
   else{document.getElementById("EyesText").innerHTML =
-    `​<p><h3>Eyeliner</h3></p> <img src="ColorPopHighlight.png"> !!!! Flower Beauty on your mark liquid eyeliner $6,compare to Stilas stay all day waterproof eyeliner $22 <br> <img src="ColorPopHighlight.png"> !!!! Elf eyeliner pen $2,compare to Stilas stay all day waterproof eyeliner $22
-    ​<p><h3>Eyebrows​</p></h3> <img src="ColorPopHighlight.png"> !!!! NYX Microbrow pencil $10,compared to Anastasia Brow Wiz $21​<br> <img src="ColorPopHighlight.png"> !!!! NYX Pomade 6.99$, compared to Anastasia Beverly Hills brow Pomade $18
-    ​<p><h3>Mascara</p></h3> <img src="ColorPopHighlight.png"> !!!! Loreal paris Voluminous Carbon Black $6, compared to the Diorshow ($27.50)
-    <p><h3>Eyeshadow and Extra</p></h3> <img src="ColorPopHighlight.png"> !!!! Milano eye shadow primer $6.50, compared to urban decay primer potion $20<br> <img src="ColorPopHighlight.png"> !!!! Makeup Academys Nude Shadows 10$,compared to Urban Decays Naked Palette $54​` ;}
+    `​<p><h3>Eyeliner</h3></p> <img src="FlowerBeautyEyeliner.jpg"> Flower Beauty on your mark liquid eyeliner $6,compare to Stilas stay all day waterproof eyeliner $22 <br> <img src="ElfPen.jpg"> Elf eyeliner pen $2,compare to Stilas stay all day waterproof eyeliner $22
+    ​<p><h3>Eyebrows​</p></h3> <img src="NYXMicrobrow.jpg"> NYX Microbrow pencil $10,compared to Anastasia Brow Wiz $21​<br> <img src="NYXPomade.jpg"> NYX Pomade 6.99$, compared to Anastasia Beverly Hills brow Pomade $18
+    ​<p><h3>Mascara</p></h3> <img src="LorealParis.jpg"> Loreal paris Voluminous Carbon Black $6, compared to the Diorshow ($27.50)
+    <p><h3>Eyeshadow and Extra</p></h3> <img src="MilaniEye.jpg"> Milano eye shadow primer $6.50, compared to urban decay primer potion $20<br> <img src="W7Buff.jpg">  W7 In The Buff Palette in Lightly Toasted $8 compared to the naked palette $54​` ;}
   }
