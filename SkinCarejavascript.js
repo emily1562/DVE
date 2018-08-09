057adcd3ec5c12ecc5d06e11d394d724ec97ef64
emily1562 <41652952+emily1562@users.noreply.github.com>
2018-08-09 12:21:13 -0700

    EMily Updates

--- SkinCarejavascript.js
+++ SkinCarejavascript.js
@@ -12,12 +12,63 @@ function SurveyButton() {
 // function makeDarkGrey(x){
 //   x.style.backgroundColor= "#8585ad"; //Dark Grey color//
 // }
-//test
 
-function FunctionTest() {
-  if (document.getElementById("TestText").innerHTML != ""){
- document.getElementById("TestText").innerHTML = "";
-  }
-else{document.getElementById("TestText").innerHTML = "test";
+var SkinType = "test";
+var Concern = "";
+
+function Oily(){
+SkinType = "Oily";
 }
+
+function Dry(){
+SkinType = "Dry";
+}
+
+function Combination(){
+SKinType = "Combination";
+}
+
+function AcneBlemishes(){
+Concern = "Acne & Blemishes";
+}
+
+function AntiAging(){
+  Concern = "Anti-Aging";
+}
+
+function Others(){
+  Concern = "Others (Pores, Dullness, etc.)";
+}
+
+function Submit(){
+  if (SkinType == "Oily" && Concern == "Acne & Blemishes") {
+    document.getElementById("Results").innerHTML = "oil, acne and blemishes test";
+  }
+  else if(SkinType == "Oily" && Concern == "Anti-Aging"){
+    document.getElementById("Results").innerHTML = "oil, antiaging test";
+  }
+  else if(SkinType == "Oily" && Concern == "Others"){
+    document.getElementById("Results").innerHTML = "oil, others Test";
+  }
+  else if (SkinType == "Dry" && Concern == "Acne & Blemishes") {
+      document.getElementById("Results").innerHTML = "Dry, acne and blemishes test";
+  }
+  else if(SkinType == "Dry" && Concern == "Anti-Aging"){
+      document.getElementById("Results").innerHTML = "Dry, AntiAging Test";
+  }
+  else if(SkinType == "Dry" && Concern == "Others"){
+      document.getElementById("Results").innerHTML = "Dry, others Test";
+  }
+  else if (SkinType == "Combination" && Concern == "Acne & Blemishes") {
+    document.getElementById("Results").innerHTML = "combination, acne and blemishes test";
+  }
+  else if(SkinType == "Combination" && Concern == "Anti-Aging"){
+    document.getElementById("Results").innerHTML = "Dry, AntiAging Test";
+  }
+  else if (SkinType == "Oily" && Concern == "Others") {
+      document.getElementById("Results").innerHTML = "Combination, others Test";
+  }
+  else {
+      document.getElementById("Results").innerHTML = "end" + SkinType + Concern;
+  }
 }
