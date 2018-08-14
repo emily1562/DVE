5d98c379a662e18980fc4b5adbf68ac0612756a4
emily1562 <41652952+emily1562@users.noreply.github.com>
2018-08-14 16:21:11 -0700

    a

--- testitoutjavascript.js
+++ testitoutjavascript.js
@@ -21,35 +21,34 @@ $(document).ready(function() {
   var lc = LC.init(
     document.getElementsByClassName('literally images-in-drawing')[0]);
   var newImage = new Image()
-  newImage.src = '/_static/sample_image.png';
+  newImage.src = 'LightSkinColor.png';
   lc.saveShape(LC.createShape('Image', {x: 10, y: 10, image: newImage}));
 });
 
-// adding images
 $(document).ready(function() {
-   var backgroundImage = new Image()
-   backgroundImage.src = 'LightSkinColor.png';
+    var backgroundImage = new Image();
+    backgroundImage.src = 'LightSkinColor.png';
 
-   var lc = LC.init(
-       document.getElementsByClassName('literally backgrounds')[0],
-       {
-         backgroundShapes: [
-           LC.createShape(
-             'Image', {x: 20, y: 20, image: backgroundImage, scale: 2}),
-           LC.createShape(
-             'Text', {
-               x: 200, y: 200, text: "I am in the background",
-               font: "bold 12px Helvetica"
-             })
-         ]
-       });
-   // the background image is not included in the shape list that is
-   // saved/loaded here
-   var localStorageKey = 'drawing-with-background'
-   if (localStorage.getItem(localStorageKey)) {
-     lc.loadSnapshotJSON(localStorage.getItem(localStorageKey));
-   }
-   lc.on('drawingChange', function() {
-     localStorage.setItem(localStorageKey, lc.getSnapshotJSON());
-   });
- });
+    var lc = LC.init(
+        document.getElementsByClassName('literally backgrounds')[0],
+        {
+          backgroundShapes: [
+            LC.createShape(
+              'Image', {x: 20, y: 20, image: backgroundImage, scale: 2}),
+            LC.createShape(
+              'Text', {
+                x: 200, y: 200, text: "I am in the background",
+                font: "bold 12px Helvetica"
+              })
+          ]
+        });
+    // the background image is not included in the shape list that is
+    // saved/loaded here
+    var localStorageKey = 'drawing-with-background'
+    if (localStorage.getItem(localStorageKey)) {
+      lc.loadSnapshotJSON(localStorage.getItem(localStorageKey));
+    }
+    lc.on('drawingChange', function() {
+      localStorage.setItem(localStorageKey, lc.getSnapshotJSON());
+    });
+  });
