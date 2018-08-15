3d8e69226dc489d8ebfbcb49db3d694eb79e7fd9
Emily L <emily.l0510@outlook.com>
2018-08-15 12:55:48 -0700

    emily

--- JavaScriptMakeup.js
+++ JavaScriptMakeup.js
@@ -15,60 +15,60 @@ function FunctionFace() {
   ​<div class= "polaroid2">
   <img src="POREfessional.jpg" alt= "Benefit Face Primer" style="width:100%">
   <div class="center"><div class="container">
-  <br> Benefit Cosmetics, POREfessional Face Primer<br> Price 49$
-  <br> Find it <a href="/phttps://www.sephora.com/product/the-porefessional-face-primer-P264900?skuId=1665074&icid2=products%20grid:p264900:product">Sephora</a>.
+  <br> Benefit Cosmetics POREfessional Face Primer<br> Price $49
+  <br> Find it at <a href="/phttps://www.sephora.com/product/the-porefessional-face-primer-P264900?skuId=1665074&icid2=products%20grid:p264900:product">Sephora</a>.
   </div></div></div>
 
   <div class= "polaroid2">
   <img src="TooFacedPrimer.jpg" alt= "Too Faced, Repleneshing Face Primer" style="width:100%">
   <div class="center"><div class="container">
-  <p>Too Faced, Hangover Replenishing Face Primer<br>Price 34$
-  <br> ​Find it in <a href="https://www.sephora.com/product/hangover-replenishing-face-primer-P387589?icid2=products%20grid:p387589:product">Sephora</a>.
+  <p>Too Faced Hangover Replenishing Face Primer<br>Price $34
+  <br> ​Find it at<a href="https://www.sephora.com/product/hangover-replenishing-face-primer-P387589?icid2=products%20grid:p387589:product">Sephora</a>.
   </p></div></div></div>
 
   <h3>Foundation</h3>​<br>
   <div class= "polaroid2">
   <img src="FentyBeautyFoundation.jpg" alt= "Fenty Beauty, Matte Longwear Foundation" style="width:100%">
   <div class="center"><div class="container">
-  <br>​Fenty Beauty, Pro Filt’r Soft Matte Longwear Foundation<br>Price 34$
-  <br>Buy it at <a href="https://www.sephora.com/product/pro-filtr-soft-matte-longwear-foundation-P87985432?skuId=1925304&icid2=products%20grid:p87985432:product">Sephora</a>.
+  <br>​Fenty Beauty Pro Filt’r Soft Matte Longwear Foundation<br>Price $34
+  <br>Find it at <a href="https://www.sephora.com/product/pro-filtr-soft-matte-longwear-foundation-P87985432?skuId=1925304&icid2=products%20grid:p87985432:product">Sephora</a>.
 </div></div></div>
 
 <div class= "polaroid2">
   <br><br><img src="BornThisWayFoundation.jpg"  alt= "Too Faced, Born this way foundation" style="width:100%">
   <div class="center"><div class="container">
-  <br>Too Faced, Born This Way Foundation<br>Price 39$ <br>
-  Buy it at <a href="https://www.sephora.com/product/born-this-way-P397517?skuId=1711910&icid2=products%20grid:p397517:product">Sephora</a>.
+  <br>Too Faced Born This Way Foundation<br>Price $39 <br>
+  Find it at <a href="https://www.sephora.com/product/born-this-way-P397517?skuId=1711910&icid2=products%20grid:p397517:product">Sephora</a>.
   </div></div></div>
 
   <p><h3>Concealer</h3>​<br>
   <div class= "polaroid2">
   <img src="NarsConcealer.jpg" alt= "Nars, radiant concealer" style="width:100%">
   <div class="center"><div class="container">
-  <br> Nars, Radiant Creamy Concealer<br>Price 30$
-  <br>​Buy it at <a href="https://www.sephora.com/product/radiant-creamy-concealer-P377873?icid2=products%20grid:p377873:product">Sephora</a>.
+  <br> Nars Radiant Creamy Concealer<br>Price $30
+  <br>​Find it at <a href="https://www.sephora.com/product/radiant-creamy-concealer-P377873?icid2=products%20grid:p377873:product">Sephora</a>.
   </div></div></div>
 
   <div class= "polaroid2">
   <br><br><img src="KatVonDConcealer.jpg" alt= "Kat Von D, Lock-IT Concealer" style="width:100%">
   <div class="center"><div class="container">
-  <br> Kat Von D, Lock- It Concealer<br>Price 26$
-  <br>​Buy it at <a href="https://www.sephora.com/product/lock-it-concealer-creme-P409985?icid2=products%20grid:p409985:product">Sephora</a>.
+  <br> Kat Von D Lock- It Concealer<br>Price $26
+  <br>​Find it at <a href="https://www.sephora.com/product/lock-it-concealer-creme-P409985?icid2=products%20grid:p409985:product">Sephora</a>.
   </div></div></div>
 
   <p><h3>Powders</h3>​<a>
   <div class= "polaroid2">
   <img src="BareMineralsFinishingPowder.jpg" alt= "Bare Minerals Finishing Powder" style="width:100%">
   <div class="center"><div class="container">
-  <br>Bare Minerals, bareSkin Perfecting Veil Finishing Powder<br> Price 27$
-  <br>​Buy it at <a href="https://www.sephora.com/product/bareskin-tm-perfecting-veil-P389226?icid2=products%20grid:p389226:product">Sephora</a>.
+  <br>Bare Minerals bareSkin Perfecting Veil Finishing Powder<br> Price $27
+  <br>​Find it at <a href="https://www.sephora.com/product/bareskin-tm-perfecting-veil-P389226?icid2=products%20grid:p389226:product">Sephora</a>.
   </div></div></div>
 
   <div class= "polaroid2">
   <br><br><img src="LauraMercierPowder.png" alt= "Laura Mercier, Translucent Loose Setting Powder" style="width:100%">
   <div class="center"><div class="container">
-  <br>Laura Mercier, Translucent Loose Setting Powder Price 38$
-  <br>​Buy it at <a href="https://www.sephora.com/product/translucent-loose-setting-powder-P109908?icid2=products%20grid:p109908:product">Sephora</a>.
+  <br>Laura Mercier, Translucent Loose Setting Powder Price $38
+  <br>​Find it at <a href="https://www.sephora.com/product/translucent-loose-setting-powder-P109908?icid2=products%20grid:p109908:product">Sephora</a>.
   </div></div></div>
 `
   if (document.getElementById("TextMakeup").innerHTML == ""){
@@ -85,15 +85,15 @@ function FunctionCheeks() {
   <div class= "polaroid2">
   <img src="BeccaShimmer.jpg" alt= "Becca, Shimmering Skin Perfector, Pressed Highlighter" style="width:100%">
   <div class="center"><div class="container">
-  <br>Becca, Shimmering Skin Perfector, Pressed Highlighter<br>Price 38$
-  <br>​Buy it at <a href="https://www.sephora.com/product/shimmering-skin-perfector-pressed-P381176?icid2=products%20grid:p381176:product">Sephora</a>.
+  <br>Becca, Shimmering Skin Perfector, Pressed Highlighter<br>Price $38
+  <br>​Find it at <a href="https://www.sephora.com/product/shimmering-skin-perfector-pressed-P381176?icid2=products%20grid:p381176:product">Sephora</a>.
   </div></div></div>
 
   <div class= "polaroid2">
   <br><img src="FentyBeautyHighlighter.jpg" alt= "Fenty Beauty, Killawatt Freestyle Highlighter" style="width:100%">
   <div class="center"><div class="container">
-  <br>Fenty Beauty, Killawatt Freestyle Highlighter<br>Price 34$
-  <br>​Buy it at <a href="https://www.sephora.com/product/killawatt-freestyle-highlighter-P64879845?icid2=products%20grid:p64879845:product
+  <br>Fenty Beauty, Killawatt Freestyle Highlighter<br>Price $34
+  <br>​Find it at <a href="https://www.sephora.com/product/killawatt-freestyle-highlighter-P64879845?icid2=products%20grid:p64879845:product
   ">Sephora</a>.
   </div></div></div>
 
@@ -101,23 +101,23 @@ function FunctionCheeks() {
 <div class= "polaroid2">
 <img src="MarcJacobsBronzer.jpg" alt= "Marc Jacobs Beauty, Mega Bronzer" style="width:100%">
 <div class="center"><div class="container">
-<br>Marc Jacobs Beauty, Mega Bronzer<br>Price 49$
-<br>​Buy it at <a href="https://www.sephora.com/product/shimmering-skin-perfector-pressed-P381176?icid2=products%20grid:p381176:product">Sephora</a>.
+<br>Marc Jacobs Beauty, Mega Bronzer<br>Price $49
+<br>​Find it at <a href="https://www.sephora.com/product/shimmering-skin-perfector-pressed-P381176?icid2=products%20grid:p381176:product">Sephora</a>.
 </div></div></div>
 
 <div class= "polaroid2">
 <br><br> <img src="KatVonDContour.jpg" alt= "Kat Von D Face Contour Palette" style="width:100%">
 <div class="center"><div class="container">
-<br>Kat Von D Face Contour Palette<br>Price 49$
-<br>​Buy it at <a href="https://www.sephora.com/product/shade-light-face-contour-refillable-palette-P413458?skuId=1848522&icid2=products%20grid:p413458">Sephora</a>.
+<br>Kat Von D Face Contour Palette<br>Price $49
+<br>​Find it at <a href="https://www.sephora.com/product/shade-light-face-contour-refillable-palette-P413458?skuId=1848522&icid2=products%20grid:p413458">Sephora</a>.
 </div></div></div>
 
 <p><h3>Blush</h3>​</p>
 <div class= "polaroid2">
 <img src="NarsBlush.jpg" alt= "Nars, Blush" style="width:100%">
 <div class="center"><div class="container">
-<br>Nars, Blush Price 30$
-<br>​Buy it at <a href="https://www.sephora.com/product/blush-P2855?skuId=1900190&icid2=products%20grid:p2855">Sephora</a>.
+<br>Nars Blush Price $30
+<br>​Find it at <a href="https://www.sephora.com/product/blush-P2855?skuId=1900190&icid2=products%20grid:p2855">Sephora</a>.
 </div></div></div>
   `
   if (document.getElementById("TextMakeup").innerHTML == ""){
@@ -132,15 +132,15 @@ function FunctionCheeks() {
     <div class= "polaroid2">
     <img src="AnastasiaLL.jpg" alt= "Anastasia, Liquid Lipstick" style="width:100%">
     <div class="center"><div class="container">
-    <br>Anastasia, Liquid Lipstick<br>Price 20$
-    <br>​Buy it at <a href="https://www.sephora.com/product/liquid-lipstick-P404831?skuId=2036390&icid2=products%20grid:p404831:product">Sephora</a>.
+    <br>Anastasia, Liquid Lipstick<br>Price $20
+    <br>​Find it at <a href="https://www.sephora.com/product/liquid-lipstick-P404831?skuId=2036390&icid2=products%20grid:p404831:product">Sephora</a>.
     </div></div></div>
 
     <div class= "polaroid2">
     <br><br><img src="FentybeautyLipGloss.jpg" alt= "Fenty beauty, Lip Gloss" style="width:100%">
     <div class="center"><div class="container">
-    <br>Fenty beauty, Lip Gloss<br>Price 18$
-    <br>​Buy it at <a href="https://www.sephora.com/product/gloss-bomb-universal-lip-luminizer-P67988453?icid2=products%20grid:p67988453:product">Sephora</a>.
+    <br>Fenty beauty, Lip Gloss<br>Price $18
+    <br>​Find it at <a href="https://www.sephora.com/product/gloss-bomb-universal-lip-luminizer-P67988453?icid2=products%20grid:p67988453:product">Sephora</a>.
     </div></div></div>
 `
     if (document.getElementById("TextMakeup").innerHTML == ""){
@@ -154,45 +154,45 @@ function FunctionCheeks() {
     <div class= "polaroid2">
     <img src="ABHGlamEyeshadow.jpg" alt= "Anastasia Beverly Hills, Soft Glam Eyeshadow Palette" style="width:100%">
     <div class="center"><div class="container">
-    <br>Anastasia Beverly Hills, Soft Glam Eyeshadow Palette<br> Price 42$
-    <br>​Buy it at <a href="https://www.sephora.com/product/soft-glam-eyeshadow-palette-P58930227?icid2=products%20grid:p58930227:product">Sephora</a>.
+    <br>Anastasia Beverly Hills, Soft Glam Eyeshadow Palette<br> Price $42
+    <br>​Find it at <a href="https://www.sephora.com/product/soft-glam-eyeshadow-palette-P58930227?icid2=products%20grid:p58930227:product">Sephora</a>.
     </div></div></div>
 
     <div class= "polaroid2">
     <br><br><img src="HudaEyeshadow.jpg" alt= "Huda Beauty, Eyeshadow Palette" style="width:100%">
     <div class="center"><div class="container">
-    <br>Huda Beauty, Eyeshadow Palette<br>Price 27$
-    <br>​Buy it at <a href="https://www.sephora.com/product/obsessions-eyeshadow-palette-P425909?icid2=products%20grid:p425909:product">Sephora</a>.
+    <br>Huda Beauty, Eyeshadow Palette<br>Price $27
+    <br>​Find it at <a href="https://www.sephora.com/product/obsessions-eyeshadow-palette-P425909?icid2=products%20grid:p425909:product">Sephora</a>.
     </div></div></div>
 
     <p><h3>Eyeliner/ Mascara</h3>​</p>​
     <div class= "polaroid2">
     <img src="KatVonDEyeliner.jpg" alt= "Kat Von D, Tattoo Eyeliner" style="width:100%">
     <div class="center"><div class="container">
-    <br>Kat Von D, Tattoo Eyeliner<br>Price 20$
-    <br>​Buy it at <a href="https://www.sephora.com/product/tattoo-liner-P245205?icid2=products%20grid:p245205:product">Sephora</a>
+    <br>Kat Von D, Tattoo Eyeliner<br>Price $20
+    <br>​Find it at <a href="https://www.sephora.com/product/tattoo-liner-P245205?icid2=products%20grid:p245205:product">Sephora</a>
     </div></div></div>
 
     <div class= "polaroid2">
     <br><br><img src="BenefitCosmeticsM.jpg" alt= "Benefit Cosmetics, They’re Real! Lengthening and Volumizing Mascara" style="width:100%">
     <div class="center"><div class="container">
-    <br>Benefit Cosmetics, They’re Real! Lengthening and Volumizing Mascara<br>Price 24$
-    <br>​Buy it at <a href="https://www.sephora.com/product/they-re-real-lengthening-volumizing-mascara-P289307?icid2=products%20grid:p289307:product">Sephora</a>
+    <br>Benefit Cosmetics, They’re Real! Lengthening and Volumizing Mascara<br>Price $24
+    <br>​Find it at <a href="https://www.sephora.com/product/they-re-real-lengthening-volumizing-mascara-P289307?icid2=products%20grid:p289307:product">Sephora</a>
     </div></div></div>
 
     <p><h3>Eyeshadow</h3>​</p>​
     <div class= "polaroid2">
     <img src="BrowWiz.jpg" alt= "Anastasia Beverly Hills, Brow Wiz" style="width:100%">
     <div class="center"><div class="container">
-    <br>Anastasia Beverly Hills, Brow Wiz<br>Price 21$
-    <br>​Buy it at <a href="https://www.sephora.com/product/brow-wiz-P202633?icid2=products%20grid:p202633:product">Sephora</a>
+    <br>Anastasia Beverly Hills, Brow Wiz<br>Price $21
+    <br>​Find it at <a href="https://www.sephora.com/product/brow-wiz-P202633?icid2=products%20grid:p202633:product">Sephora</a>
     </div></div></div>
 
     <div class= "polaroid2">
     <br><img src="ABHPomade.jpg"  alt= "Anastasia Beverly Hills, Eyebrow Dipbrow Pomade" style="width:100%">
     <div class="center"><div class="container">
-    <br>Anastasia Beverly Hills, Eyebrow Dipbrow Pomade<br>Price 18$
-    <br>​Buy it at <a href="https://www.sephora.com/product/dipbrow-pomade-P384060?icid2=top-rated%20makeup:p384060:product">Sephora</a>
+    <br>Anastasia Beverly Hills, Eyebrow Dipbrow Pomade<br>Price $18
+    <br>​Find it at <a href="https://www.sephora.com/product/dipbrow-pomade-P384060?icid2=top-rated%20makeup:p384060:product">Sephora</a>
     </div></div></div>`
     if (document.getElementById("TextMakeup").innerHTML == ""){
     document.getElementById("TextMakeup").innerHTML = Eyes;
